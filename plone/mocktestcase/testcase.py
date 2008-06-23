89a988b37776ccffea0684c1bd9042c488200b2c
Martin Aspeli <optilude@gmail.com>
2008-06-23 12:37:17 +0000

    Initial checkin. This works though there are not many examples.
    svn path=/plone.mocktestcase/trunk/; revision=21232

---
+++ plone/mocktestcase/testcase.py
@@ -0,0 +1,58 @@
+import mocker
+
+import zope.component
+import zope.component.testing
+
+class MockTestCase(mocker.MockerTestCase):
+    """Base class for mocker-based mock tests. There are convenience methods
+    to make mock testing easier.
+    """
+    
+    # Ensure that we tear down the CA after each test method
+
+    def tearDown(self):
+        super(MockTestCase, self).tearDown()
+        zope.component.testing.tearDown(self)
+    
+    # For the lazy
+    
+    def replay(self):
+        self.mocker.replay()
+    
+    # Help register mock components. The tear-down method will
+    # wipe the registry each time.
+    
+    def mock_utility(self, mock, provides, name=u""):
+        """Register the mock as a utility providing the given interface 
+        """
+        zope.component.provideUtility(provides=provides, component=mock, name=name)
+    
+    def mock_adapter(self, mock, provides, adapts, name=u""):
+        """Register the mock as an adapter providing the given interface 
+        and adapting the given interface(s)
+        """
+        zope.component.provideAdapter(factory=mock, adapts=adapts, provides=provides, name=name)
+    
+    def mock_subscription_adapter(self, mock, provides, adapts):
+        """Register the mock as a utility providing the given interface 
+        """
+        zope.component.provideSubscriptionAdapter(factory=mock, provides=provides, adapts=adapts)
+    
+    def mock_handler(self, mock, adapts):
+        """Register the mock as a utility providing the given interface 
+        """
+        zope.component.provideHandler(factory=mock, adapts=adapts)
+    
+    # Matcher functions
+    
+    def match_provides(self, interface):
+        """A function parameter matches that checks whether the given
+        interface is provided by the function argument, e.g.
+
+            some_mock = self.mocker.mock()
+            some_mock.foo(self.match_provides(IFoo))
+        
+        This will ensure that foo() is called on some_mock with an object
+        that provides IFoo.
+        """
+        return mocker.MATCH(lambda x: interface.providedBy(x))
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+setup(name='plone.mocktestcase',
+      version=version,
+      description="Mock unit test case based on ``mocker``",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Martin Aspeli',
+      author_email='optilude@gmail.com',
+      url='http://plone.org',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'mocker',
+          # 'zope.component',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
